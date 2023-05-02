313a47d361fafbedfc18a56e7b2a12a4d36fe5b1
Pamela Gomes <pamela.gomes.work@gmail.com>
2023-05-03 00:41:05 +0200

    Updtes from codes

--- areas_of_shapes.py
+++ areas_of_shapes.py
@@ -1,4 +1,4 @@
-print("Areas and Shapes!")
+print('"Areas and Shapes!"')
 #Area of a square
 side_square = float(input("What is the length of a side of square?"))
 area_1 = side_square ** 2
@@ -20,3 +20,8 @@ import math
 radius = float(input("What is the radius of the circle?"))
 area_3 = math.pi * (radius ** 2)
 print("The area of a circle is: " + str(area_3))
+
+
+# radius = float(input("What is the radius of the circle?"))
+# area_3 = 3.14159 * (radius ** 2)
+# print("The area of a circle is: " + str(area_3))
